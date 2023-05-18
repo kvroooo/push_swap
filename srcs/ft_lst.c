50349f8dece6311d9bb60f234a152ecd6104e225
Samira <kvrosam@gmail.com>
2023-05-18 18:00:10 +0200

    tri3 v2

--- srcs/ft_lst.c
+++ srcs/ft_lst.c
@@ -6,7 +6,7 @@
 /*   By: smlamali <smlamali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 17:38:10 by smlamali          #+#    #+#             */
-/*   Updated: 2023/05/18 17:00:13 by smlamali         ###   ########.fr       */
+/*   Updated: 2023/05/18 17:57:50 by smlamali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,26 @@ t_data	*create_cell(int nbr)
 	tmp->next = NULL;
 	return (tmp);
 }
+
+// t_data	*set_at(t_data *get_data, t_data *set_data, int pos)
+// {
+// 	int		i;
+// 	t_data	*prev;
+// 	t_data	*next;
+// 	t_data	*current;
+
+// 	i = 0;
+// 	prev = data;
+// 	current = set_data;
+// 	if (!data)
+// 		return (NULL);
+// 	if (pos == 0)
+// 	{
+// 		current->next = get_data;
+// 		return (current);
+// 	}
+// 	return (NULL);
+// }
 
 //add at position, data
 t_data	*add_at(t_data *data, int nb, int pos)
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: smlamali <smlamali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 18:15:39 by kuro              #+#    #+#             */
-/*   Updated: 2023/05/18 17:20:29 by smlamali         ###   ########.fr       */
+/*   Updated: 2023/05/18 17:59:20 by smlamali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,8 @@ int	main(int argc, char *argv[])
 		return (ft_error("pile null"), 0);
 	ft_printf("len: %d\n", pile->len);
 	print_list(pile->a);
-	smoll_sort(pile);
+	if (argc <= 4)
+		smoll_sort(pile);
 	print_list(pile->a);
 	ft_printf("------ END ------\n");
 	free_data(pile->a);
